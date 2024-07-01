08fe4ed4d3a466a1806c8d266478bbf157e00e34
braitcode <brightjohn489@gmail.com>
2024-07-01 11:00:42 +0100

    new commit

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -10,9 +10,9 @@ const Skill = () => {
         <div className="overflow-hidden xl:w-full xl:h-[100vh]  p-8 xl:flex xl:flex-col xl:justify-center gap-[106px]">
               <div className="container xl:flex xl:flex-col md:flex md:flex-col md:justify-center  gap-24 xl:w-[100%]">
 
-                  <div className="xl:w-[1024px] md:w-full xl:h-[130.33px] xl:gap-[21.33px] md:flex md:flex-col md:justify-center md:items-center">
+                  <div className="xl:w-[1024px] md:w-full xl:h-[130.33px] xl:gap-[21.33px] md:flex md:flex-col md:justify-center ">
                       <p className='font-[Roboto] font-semibold xl:text-[21.33px]'>My Skills</p>
-                      <h1 className='font-[Roboto] xl:text-[4rem] text-[3rem] font-bold'>My Expertise</h1>
+                      <h1 className='font-[Roboto] xl:text-[4rem] md:text-[4rem] text-[3rem] font-bold'>My Expertise</h1>
                   </div>
                   <div className="xl:flex xl:flex-row xl:justify-center xl:gap-10 xl:w-[100%] md:w-full md:flex md:justify-center md:items-center flex flex-col gap-8">
                     
@@ -20,28 +20,28 @@ const Skill = () => {
                           <img src={img1} alt="" className='w-[79.33px] h-[79.33px] mt-4' />
                           <div className="w-[341.33px] h-[194.33px] md:w-full">
                               <h3 className='text-[28px] font-bold text-[#282938] my-4 font-[Roboto]'>Responsive Design</h3>
-                              <p className='font-[Roboto] font-normal leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                              <p className='font-[Roboto] font-normal xl:leading-[1.5rem] leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                           </div>
                       </div>
-                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[350px] shadow-md rounded-md p-2">
+                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[3650x] rounded-md shadow-md p-2 ">
                           <img src={img2} alt="" className='w-[79.33px] h-[79.33px] mt-4' />
                           <div className="w-[341.33px] h-[194.33px] md:w-full">
                               <h3 className='text-[28px] font-bold text-[#282938] my-4 font-[Roboto]'>Front-End Frameworks</h3>
-                              <p className='font-[Roboto] font-normal leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                              <p className='font-[Roboto] font-normal xl:leading-[1.5rem] leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                           </div>
                       </div>
-                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[350px] shadow-md rounded-md p-2">
+                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[3650x] rounded-md shadow-md p-2">
                           <img src={img3} alt="" className='w-[79.33px] h-[79.33px] mt-4' />
                           <div className="w-[341.33px] h-[194.33px] md:w-full">
-                              <h3 className='text-[28px] font-bold text-[#282938] my-4 font-[Roboto]'>Testing and Debugging</h3>
-                              <p className='font-[Roboto] font-normal leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                              <h3 className='text-[28px] font-bold text-[#282938] my-4 font-[Roboto]'>Testing and Debuggin</h3>
+                              <p className='font-[Roboto] font-normal xl:leading-[1.5rem] leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                           </div>
                       </div>
-                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[350px] shadow-md rounded-md p-2">
+                      <div className="bg-[#F5FCFF] w-[340.33px] md:w-full h-[3650x] rounded-md shadow-md p-2">
                           <img src={img4} alt="" className='w-[79.33px] h-[79.33px] mt-4' />
                           <div className="w-[341.33px] h-[194.33px] md:w-full">
                               <h3 className='text-[28px] font-bold text-[#282938] my-4 font-[Roboto]'>Cloud Services</h3>
-                              <p className='font-[Roboto] font-normal leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                              <p className='font-[Roboto] font-normal xl:leading-[1.5rem] leading-[1.5rem] md:leading-[2.5rem] md:w-[90%] xl:w-[90%] xl:text-[18px] md:text-[40px]  text-[#1C1E53]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                           </div>
                       </div>
 
--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -9,19 +9,19 @@ const Testimonials = () => {
   return (
     <div>
         <div className="overflow-hidden bg-[#F5FCFF] w-full xl:h-[902.18px] p-8 xl:flex xl:flex-col xl:justify-center xl:items-center flex flex-col gap-10 xl:gap-[106.67px]">
-            <div className="w-full h-[130.33px] gap-[21.33px] flex justify-between items-center">
+            <div className="w-full h-[130.33px] gap-[21.33px] xl:flex xl:flex-row xl:justify-between xl:items-center md:flex md:flex-row md:justify-between md:items-center flex flex-col">
                   <div className="">
                       <p className='font-[Roboto] font-semibold text-[21.33px]'>Clients Feedback</p>
                       <h1 className='font-[Roboto] xl:text-[4rem] text-[2rem] font-bold'>Customer Testimonials</h1>
                   </div>
             </div>
             
-            <div className="xl:flex xl:flex-row xl:justify-center xl:items-center flex flex-col gap-8 w-full xl:gap-10">
-                <div className="xl:w-[512px] xl:h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
+            <div className=" xl:flex xl:flex-row xl:justify-center xl:items-center flex flex-col gap-8 w-full xl:gap-10">
+                <div className="xl:w-[400px] xl:h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
                     <div className="">
                     <img src={stars} alt="" className='w-[154px.67px] h-[25.19px]' />
                     </div>
-                    <p className='xl:w-[450.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
+                    <p className='xl:w-[280.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
                     <div className="flex items-center gap-6">
                         <img src={dianne} alt="" className='w-[74.67px] h-[74.67px]' />
                         <div className="">
@@ -30,11 +30,11 @@ const Testimonials = () => {
                         </div>
                     </div>
                 </div>
-                <div className="xl:w-[512px] h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
+                <div className="xl:w-[400px] h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
                     <div className="">
                     <img src={stars} alt="" className='w-[154px.67px] h-[25.19px]' />
                     </div>
-                    <p className='xl:w-[450.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
+                    <p className='xl:w-[280.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
                     <div className="flex items-center gap-6">
                         <img src={kristin} alt="" className='w-[74.67px] h-[74.67px]' />
                         <div className="">
@@ -43,11 +43,11 @@ const Testimonials = () => {
                         </div>
                     </div>
                 </div>
-                <div className="xl:w-[512px] h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
+                <div className="xl:w-[400px] h-[366.26px] p-11 flex flex-col gap-10 border border-[#006B6A] rounded-md">
                     <div className="">
                     <img src={stars} alt="" className='w-[154px.67px] h-[25.19px]' />
                     </div>
-                    <p className='xl:w-[450.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
+                    <p className='xl:w-[280.33px]'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
                     <div className="flex items-center gap-6">
                         <img src={kathryn} alt="" className='w-[74.67px] h-[74.67px]' />
                         <div className="">
